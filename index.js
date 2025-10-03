a56f27c8c1cdd7b24b739147ee8f2099cad20a30
AmrutaBilagi <amrutabilagi8@gmal.com>
2025-10-03 17:33:15 +0530

    third commit

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Backend development')
 })
+app.get('/youtube',(req,res)=>{
+  res.send("you tube")
+})
+app.get('/twitter',(req,res)=>{
+  res.send("MY TWITTER")
+})
+app.get('/Github',(req,res)=>{
+  res.send("GitHub")
+})
+app.get('/Instagram',(req,res)=>{
+  res.send("Instagram")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
